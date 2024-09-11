2589f2f5f388f6e90977d968ca13e58045b11f8d
Muhammed-AbdelRaheem <MuhammedAbdelRaheem93@gmail.com>
2024-09-11 21:52:23 +0300

    Add project files.

---
+++ Company.G02.DAL/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Company.G02.DAL.Data.Configurations;
+using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Contexts
+{
+    internal class AppDbContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server= . ; DataBase =  CompanyG02 ; Trusted_Connection =true;TrustServerCertificate=true");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.ApplyConfiguration(new DepartmentConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
